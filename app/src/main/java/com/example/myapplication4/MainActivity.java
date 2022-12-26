f3ccf4562a38a0191123f4ffeafc236a67bed4ec
Was404 <vladimirovichdanil@bk.ru>
2022-12-27 00:51:45 +0300

    added maket settings with url github

--- app/src/main/java/com/example/myapplication4/MainActivity.java
+++ app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button_menu, button_teorver, button_opersys;
+    ImageButton settings_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         button_menu = (Button) findViewById(R.id.button_menu);
         button_teorver = (Button) findViewById(R.id.button_teorver);
         button_opersys = (Button) findViewById(R.id.button_opersys);
+        settings_btn = (ImageButton) findViewById(R.id.settings_btn);
 
 
         button_teorver.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, OperatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settings_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettigsActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/myapplication4/SettigsActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettigsActivity extends AppCompatActivity {
+    TextView mygithub;
+    Button button_back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mygithub = (TextView) findViewById(R.id.mygithub);
+        button_back = (Button) findViewById(R.id.button_back);
+        String textToFirsturl = "Project on github";
+        SpannableString ss = new SpannableString(textToFirsturl);
+        ss.setSpan(new URLSpan(String.valueOf(mygithub)), 0, mygithub.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mygithub.setText(ss);
+
+        String dynamicUrl = "https://github.com/Was404/Allinone_android";
+        String linkedText = String.format(
+                "<a href=\"%s\">https://github.com/Was404/Allinone_android</a> ", dynamicUrl);
+        mygithub.setText(Html.fromHtml(linkedText));
+        mygithub.setMovementMethod(LinkMovementMethod.getInstance());
+
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettigsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
